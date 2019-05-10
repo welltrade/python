380c728625728ba65b28e450697c4b444837430a
welltrade <42456703+welltrade@users.noreply.github.com>
2019-05-10 16:11:23 +0300

    Update rnd_price_genrtr_matplot.py
    add comments

--- rnd_price_genrtr_matplot.py
+++ rnd_price_genrtr_matplot.py
@@ -1,16 +1,23 @@
 import random
 import matplotlib.pyplot as plt
 
+#how much price items
 l = 20
+
+#create new list
 dt =[]
+
+#add random first price to list as first item in 0 position
 x = random.randint(0, 100)
 dt.append(x)
+
+#create list of prices use while
 i = 0
 while i < l:
     dt.append(x + random.choice([-1, -1, -1, 0, 1, 1, 1,]) + random.choice([-0.5, -0.25, 0, 0.25, 0.5]) + random.choice([-0.5, -0.25, 0, 0.25, 0.5]))
     i += 1
     
-#print(dt) 
+#ceate plot
 
 plt.plot(list(range(0, l + 1)), dt, 'ro')
 plt.axis([0, l + 2 , min(dt) - 2, max(dt) + 2])
